d149615fc710087d7e0bf700894a2d7a7c7404a9
Devendradhote001 <ddhote780@gmail.com>
2025-06-18 19:24:46 +0530

    day 3 redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, incrementByAmout } from "./features/reducers/counterSlice";
+import { useForm } from "react-hook-form";
+import {
+  currentUserAction,
+  loginAction,
+  registerAction,
+} from "./features/actions/userAction";
 
 const App = () => {
   const dispatch = useDispatch();
+  const { register, handleSubmit, reset } = useForm();
   const [amt, setAmt] = useState(0);
   const count = useSelector((state) => state.counter.value);
+
+  new Promise((resolve, reject) => {});
+
+  const formSubmit = (data) => {
+    dispatch(loginAction(data));
+    reset();
+    alert("user logged in");
+  };
+
+  useEffect(() => {
+    dispatch(currentUserAction());
+  }, []);
 
   return (
     <div>
@@ -20,6 +39,17 @@ const App = () => {
       <button onClick={() => dispatch(incrementByAmout(amt))}>
         Add by amount
       </button>
+
+      <form onSubmit={handleSubmit(formSubmit)} action="">
+        {/* <input type="text" placeholder="username" {...register("name")} /> */}
+        <input type="email" placeholder="email" {...register("email")} />
+        <input
+          type="password"
+          placeholder="password"
+          {...register("password")}
+        />
+        <button>submit</button>
+      </form>
     </div>
   );
 };
--- src/features/actions/userAction.jsx
+++ src/features/actions/userAction.jsx
@@ -1,9 +1,35 @@
 import { axiosInstance } from "../../axiosInstance";
+import axios from "axios";
+import { setUser } from "../reducers/userSlice";
 
-export const registerAction = (user) => async (dispatch) => {
+export const registerAction = (user) => async () => {
   try {
-    let res = await axiosInstance.post("/users", user);
+    let res = await axios.post("http://localhost:3000/users", user);
     console.log("api res from reg action->", res);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const loginAction = (user) => {
+  return async (dispath, state) => {
+    try {
+      let res = await axiosInstance.get(
+        `/users?email=${user.email}&password=${user.password}`
+      );
+      localStorage.setItem("user", JSON.stringify(res.data[0]));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+export const currentUserAction = () => async (dispatch) => {
+  try {
+    let storeData = JSON.parse(localStorage.getItem("user"));
+    if (storeData) {
+      dispatch(setUser(storeData));
+    }
   } catch (error) {
     console.log(error.message);
   }
